2ef8fda47c0f4320f7a601fe25a737f70d45714c
dukehootfish <77045140+dukehootfish@users.noreply.github.com>
2023-08-19 17:21:18 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
-  <title>Your Airtable Website</title>
-</head>
-<body>
-  <header>
-    <h1>CatalogFish</h1>
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Products</a></li>
-        <li><a href="#">Categories</a></li>
-      </ul>
-    </nav>
-  </header>
 
-  <main class="container" id="product-list">
-    <!-- Product listings will be dynamically generated here -->
-  </main>
-
-  <script src="scripts.js"></script>
-</body>
-</html>
-/*const PERSONAL_ACCESS_TOKEN = 'patFPhQ2NCGDqLUJR.14c2310f47d18237673940cc7428eeec8d471ffbb4510d81d099aeec97c48fae';
+const PERSONAL_ACCESS_TOKEN = 'patFPhQ2NCGDqLUJR.14c2310f47d18237673940cc7428eeec8d471ffbb4510d81d099aeec97c48fae';
 const airtableBaseId = 'appHQzwgShaDAx9Aw';
 
 async function fetchProducts() {
@@ -44,6 +18,8 @@ async function fetchProducts() {
   }
 }
 
+// ... (previous code)
+
 async function displayProducts() {
   const products = await fetchProducts();
 
@@ -53,28 +29,41 @@ async function displayProducts() {
     const { Name, Description, ImageUrl } = product.fields;
 
     const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+    productCard.classList.add('col-md-6', 'col-lg-4');
+    productCard.id = 'cat-box';
 
-    const name = document.createElement('h2');
-    name.textContent = Name;
+    const cardWrapper = document.createElement('div');
+    cardWrapper.classList.add('card', 'border-0');
+    cardWrapper.style.background = 'rgba(255, 255, 255, 0)';
 
-    const image = document.createElement('img');
-    image.src = ImageUrl;
-    image.alt = Name;
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body', 'cat-card-body');
 
-    const description = document.createElement('p');
-    description.textContent = Description;
-      
-    const itemDiv = document.createElement("div");
-            <div class="col-md-6 col-lg-4" id="cat-box">
-                <div class="card border-0" style="background: rgba(255,255,255,0);"><a></a>
-                    <div class="card-body cat-card-body"><a id="cat-name"><strong>{name}</strong></a></div>
-                </div><img class="imageicon" src="i13.png"><a class="btn btn-primary btn-sm" role="button" id="link_button" href="https://dukehootfish.github.io/catalog/catalogfish.html#c3">לרשימת הספקים &gt;</a>
-            </div>
-            `;
+    const productNameLink = document.createElement('a');
+    productNameLink.id = 'cat-name';
+    productNameLink.innerHTML = `<strong>${Name}</strong>`;
+    cardBody.appendChild(productNameLink);
 
-    productContainer.appendChild(itemDiv);
-  };
-}*/
+    cardWrapper.appendChild(cardBody);
+
+    const productImage = document.createElement('img');
+    productImage.classList.add('imageicon');
+    productImage.src = ImageUrl;
+
+    const linkButton = document.createElement('a');
+    linkButton.classList.add('btn', 'btn-primary', 'btn-sm');
+    linkButton.role = 'button';
+    linkButton.id = 'link_button';
+    linkButton.href = 'https://dukehootfish.github.io/catalog/catalogfish.html#c3';
+    linkButton.textContent = 'לרשימת הספקים >';
+
+    productCard.appendChild(cardWrapper);
+    productCard.appendChild(productImage);
+    productCard.appendChild(linkButton);
+
+    productContainer.appendChild(productCard);
+  });
+}
 
 displayProducts();
+
