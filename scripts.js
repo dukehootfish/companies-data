48d903b35805db03090a7a044c5e0c6ec65c50d3
dukehootfish <77045140+dukehootfish@users.noreply.github.com>
2023-08-19 16:47:55 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -65,11 +65,11 @@ async function displayProducts() {
     description.textContent = Description;
       
     const itemDiv = document.createElement("div");
-             <div class="col-md-6 col-lg-4" id="cat-box">
-                        <div class="card border-0" style="background: rgba(255,255,255,0);"><a></a>
-                            <div class="card-body cat-card-body"><a id="cat-name"><strong>{name}</strong></a></div>
-                        </div><img class="imageicon" src="i13.png"><a class="btn btn-primary btn-sm" role="button" id="link_button" href="https://dukehootfish.github.io/catalog/catalogfish.html#c3">לרשימת הספקים &gt;</a>
-                    </div>
+            <div class="col-md-6 col-lg-4" id="cat-box">
+                <div class="card border-0" style="background: rgba(255,255,255,0);"><a></a>
+                    <div class="card-body cat-card-body"><a id="cat-name"><strong>{name}</strong></a></div>
+                </div><img class="imageicon" src="i13.png"><a class="btn btn-primary btn-sm" role="button" id="link_button" href="https://dukehootfish.github.io/catalog/catalogfish.html#c3">לרשימת הספקים &gt;</a>
+            </div>
             `;
 
     productContainer.appendChild(itemDiv);
