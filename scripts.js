a2a8dc207f5ea94f533fed81fdf56e3ae5429e8e
dukehootfish <77045140+dukehootfish@users.noreply.github.com>
2023-08-19 17:16:44 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,30 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Your Airtable Website</title>
+</head>
+<body>
+  <header>
+    <h1>CatalogFish</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Categories</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main class="container" id="product-list">
+    <!-- Product listings will be dynamically generated here -->
+  </main>
+
+  <script src="scripts.js"></script>
+</body>
+</html>
 /*const PERSONAL_ACCESS_TOKEN = 'patFPhQ2NCGDqLUJR.14c2310f47d18237673940cc7428eeec8d471ffbb4510d81d099aeec97c48fae';
-const BASE_ID = 'appHQzwgShaDAx9Aw';
-
-const bookList = document.getElementById('bookList');
-
-async function fetchBooks() {
-    try {
-        const response = await fetch(`https://api.airtable.com/v0/${BASE_ID}/Books`, {
-            headers: {
-                Authorization: `Bearer ${PERSONAL_ACCESS_TOKEN}`,
-            },
-        });
-        const data = await response.json();
-
-        data.records.forEach((record) => {
-            const book = record.fields;
-            const li = document.createElement('li');
-            li.innerHTML = `<strong>${book.Title}</strong> by ${book.Author} (${book.Genre})`;
-            bookList.appendChild(li);
-        });
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
-
-fetchBooks();// JavaScript source code */
-const PERSONAL_ACCESS_TOKEN = 'patFPhQ2NCGDqLUJR.14c2310f47d18237673940cc7428eeec8d471ffbb4510d81d099aeec97c48fae';
 const airtableBaseId = 'appHQzwgShaDAx9Aw';
 
 async function fetchProducts() {
@@ -74,6 +75,6 @@ async function displayProducts() {
 
     productContainer.appendChild(itemDiv);
   };
-}
+}*/
 
 displayProducts();
